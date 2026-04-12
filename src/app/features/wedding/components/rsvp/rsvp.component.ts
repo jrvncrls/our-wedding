367caf5dfee7dfe70e9b56b43959656db5c77829
Jervin Carlos <jlcarloswork@gmail.com>
2026-04-12 17:52:23 +0800

    Update rsvp

--- src/app/features/wedding/components/rsvp/rsvp.component.ts
+++ src/app/features/wedding/components/rsvp/rsvp.component.ts
@@ -1,9 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, signal } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 export interface RSVPFormValue {
   attending: string;
+  stayOvernight: string;
   dietary: string;
   message: string;
 }
@@ -15,17 +17,31 @@ export interface RSVPFormValue {
   templateUrl: './rsvp.component.html',
   styleUrl: './rsvp.component.scss',
 })
-export class RsvpComponent {
+export class RsvpComponent implements OnDestroy {
   submitted = signal(false);
   submitting = signal(false);
 
   form: FormGroup;
 
+  private attendingSub: Subscription;
+
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       attending: ['', Validators.required],
+      stayOvernight: [''],
       dietary: [''],
       message: [''],
+    });
+
+    this.attendingSub = this.form.get('attending')!.valueChanges.subscribe((val) => {
+      const ctrl = this.form.get('stayOvernight')!;
+      if (val === 'yes') {
+        ctrl.setValidators(Validators.required);
+      } else {
+        ctrl.clearValidators();
+        ctrl.reset();
+      }
+      ctrl.updateValueAndValidity();
     });
   }
 
@@ -35,6 +51,14 @@ export class RsvpComponent {
 
   isNotAttending(): boolean {
     return this.form.get('attending')?.value === 'no';
+  }
+
+  isStayingOvernight(): boolean {
+    return this.form.get('stayOvernight')?.value === 'yes';
+  }
+
+  isNotStayingOvernight(): boolean {
+    return this.form.get('stayOvernight')?.value === 'no';
   }
 
   fieldInvalid(name: string): boolean {
@@ -61,5 +85,9 @@ export class RsvpComponent {
   resetForm(): void {
     this.form.reset();
     this.submitted.set(false);
+  }
+
+  ngOnDestroy(): void {
+    this.attendingSub.unsubscribe();
   }
 }
